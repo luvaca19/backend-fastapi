0685c27d98d90c6678e838ad6a6e05770791ae2b
Ludmila <luvaca1318@gmail.com>
2026-04-03 20:19:52 -0300

    agregue DELETE y PUT

--- main.py
+++ main.py
@@ -13,11 +13,27 @@ bdd = []
 
 @app.post("/usuario")
 def crear_usuario(usuario: Usuario):
-    bdd.append(usuario.dict())
+    bdd.append(usuario)
     return {"mensaje": f"el usuario fue agregado {usuario}"}
 
 @app.get("/usuarios")
 def mostrar_usuario():
     return bdd
 
+@app.delete("/usuario/{usuario_id}")
+def eliminar_usuario(usuario_id: int):
+    for usuario in bdd:
+        if usuario["id"] == usuario_id:
+            bdd.remove(usuario)
+            return {"message": "el usuario a sido eliminado"}
+        
+    return {"message": "el usuario no ha sido encontrado"}
 
+@app.put("/user/{usuario_id}")
+def modificar_usuario(usuario_id: int, usuario_actualizado: Usuario):
+    for usuario in bdd:
+        if usuario.id == usuario_id:
+            usuario.name = usuario_actualizado.name
+            return {"message": "el usuario ha sido actualizado"}
+        
+    return {"message": "el usuario no ha sido encontrado"}
